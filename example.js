d145c2a63680cf4402a00355389b614a9f36de68
Md. Masudul Kabir <masud@jrfintel.com>
2022-12-28 09:42:42 +0600

    Commit

---
+++ example.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// The reducer function updates the state based on the action that is dispatched
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    case 'DECREMENT':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+// Create a Redux store with an initial state of { count: 0 }
+const store = createStore(reducer, { count: 0 });
+
+// The Counter component reads the count from the Redux store and dispatches actions to update it
+function Counter() {
+  return (
+    <div>
+      <button onClick={() => store.dispatch({ type: 'DECREMENT' })}>-</button>
+      <span>{store.getState().count}</span>
+      <button onClick={() => store.dispatch({ type: 'INCREMENT' })}>+</button>
+    </div>
+  );
+}
+
+// The App component wraps the Counter in a Provider to make the store available to the Counter
+function App() {
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+}
+
+export default App;
